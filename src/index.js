1ef4bea35221fe59df1784ee502318e5e172fe65
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2022-10-22 18:26:51 +0200

    Complete module3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-export function fetchMovies(){};
+import { fetchWithTimeout } from "./services";
+
+const movies = require("./data/movies.json");
+
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    fetchWithTimeout(1000)
+    .then(resolveFunction())
+ };
+
+let moviePromise = fetchMovies();
+
+moviePromise
+    .then(res => console.log(res));
+
+
+
