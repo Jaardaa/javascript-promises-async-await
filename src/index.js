6814bcee98cbbb3b13c0d54aa8137fc08b63e431
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2022-10-22 20:24:24 +0200

    CompleteModule4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import { fetchWithTimeout } from "./services";
+
+import { fetchMovies, fetchBooks, fetchWithTimeout } from "./services";
 
 const movies = require("./data/movies.json");
 
-export function fetchMovies() {
-  const resolveFunction = () => movies;
-  return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+    Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ([
+            books,
+            movies
+        ]))
+        .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+getBooksAndMoviesPromise.then(
+    results => { console.log('getBooksAndMoviesPromise', results)}
+)
+
+function getBooksorMovies() {
+    Promise.race([fetchBooks(), fetchMovies()])
+      .then((results) => results)
+      .catch((error) =>
+        console.log("Error waiting for the promise race", error)
+      );
+}
+
+const getBooksOrMoviesPromise = getBooksorMovies();
+
+getBooksOrMoviesPromise.then(
+    results => {console.log("getBooksOrMoviesPromise", results);}
+)
+
+
+/* export function fetchMovies() {
+  const resolveFunction = () => movies;
+  return fetchWithTimeout(1000).then(resolveFunction);
+} */
+
+/* const moviePromise = fetchMovies();
 moviePromise.then((results) => {
   console.log(results);
-});
+}); */
