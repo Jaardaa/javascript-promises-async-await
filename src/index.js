aa2b615c95412a03b60067595e59000b126652d7
Jaardaa <78655691+Jaardaa@users.noreply.github.com>
2022-10-22 18:38:03 +0200

    CompleteModule3a

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ const movies = require("./data/movies.json");
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
-    fetchWithTimeout(1000)
-    .then(resolveFunction())
+    return fetchWithTimeout(1000).then(resolveFunction)
  };
 
-let moviePromise = fetchMovies();
+const moviePromise = fetchMovies();
 
 moviePromise
-    .then(res => console.log(res));
+    .then(results => { console.log(results); });
 
 
 
